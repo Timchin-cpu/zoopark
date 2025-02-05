bf979d71b032827aa7928dc1cb49e804f4411e0f
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-05 17:48:16 +0000

    modified:   src/pages/PeoplePage/index.jsx

--- src/pages/PeoplePage/index.jsx
+++ src/pages/PeoplePage/index.jsx
@@ -75,9 +75,7 @@ const PeoplePage = () => {
     <div className="city-slider__item" key={photo.id}>
       <div className="city-slider__card" onClick={handleOpenPopup}>
         <p className="city-slider__image">
-          <img src={photo.image
-            
-          } alt={photo.title} />
+          <img src={photo.image} alt={photo.title} />
         </p>
       </div>
     </div>
