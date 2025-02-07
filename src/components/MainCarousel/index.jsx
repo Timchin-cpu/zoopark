f0bb07f7aa63f13eec9b1cfe75effece433620f5
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-07 09:03:46 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -17,21 +17,22 @@ import { peopleService } from "services/api";
 
 import DefaultImg from "assets/img/default-card.png";
 // import ProdImg from "assets/img/prod-img.png";
-const [cardStates, setCardStates] = useState(
-  data.map(() => ({
-    isFlipped: false,
-    selectedPhoto: null,
-  }))
-);
-const MainCarousel = ({ getActiveSlide, handleOpenPopup }) => {
-  const [selectedId, setSelectedId] = useState(null);
-  const [photos, setPhotos] = useState([]); // Добавить состояние для хранения всех фото
 
+const MainCarousel = ({ getActiveSlide, handleOpenPopup }) => {
   const data = [
     { id: 1, bgColor: "#F54748", title: "Slide 1" },
     { id: 2, bgColor: "#7952B3", title: "Slide 2" },
     { id: 3, bgColor: "#1597BB", title: "Slide 3" },
   ];
+  const [cardStates, setCardStates] = useState(
+    data.map(() => ({
+      isFlipped: false,
+      selectedPhoto: null,
+    }))
+  );
+  const [selectedId, setSelectedId] = useState(null);
+  const [photos, setPhotos] = useState([]); // Добавить состояние для хранения всех фото
+
   useEffect(() => {
     const fetchPhotos = async () => {
       try {
