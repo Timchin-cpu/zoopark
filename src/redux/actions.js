fcf099dfb553bfe31a39031c76f9a62e12b2c137
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-10 14:50:34 +0000

    modified:   src/redux/actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,9 @@
 export const setTheme = (theme) => ({
-  type: 'SET_THEME',
+  type: "SET_THEME",
   payload: theme,
+});
+// redux/actions.js
+export const setCardBack = (style) => ({
+  type: "SET_CARD_BACK",
+  payload: style,
 });
