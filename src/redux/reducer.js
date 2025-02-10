c4f2fc50afc6d2b31d539f5b9fe9fea2bd9eb96d
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-10 15:44:36 +0000

    modified:   src/redux/reducer.js 	modified:   src/redux/store.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,21 +1,21 @@
-const initialState = {
-  theme: true,
-  cardBack: "default", // Добавить начальное состояние для cardBack
-};
+// const initialState = {
+//   theme: true,
+//   cardBack: "default", // Добавить начальное состояние для cardBack
+// };
 
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "SET_THEME":
-      return {
-        ...state,
-        theme: action.payload,
-      };
-    case "SET_CARD_BACK":
-      return {
-        ...state,
-        cardBack: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+// const rootReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "SET_THEME":
+//       return {
+//         ...state,
+//         theme: action.payload,
+//       };
+//     case "SET_CARD_BACK":
+//       return {
+//         ...state,
+//         cardBack: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
