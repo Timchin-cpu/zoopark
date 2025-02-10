5956fcb68d97ebc29822cd1326c2b81686e06035
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-10 15:40:40 +0000

    modified:   src/redux/store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,13 @@ import { createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./reducer";
-import { combineReducers } from "redux";
-import { cardBackReducer } from "./reducers/cardBackReducer";
+// import { combineReducers } from "redux";
+// import { cardBackReducer } from "./reducers/cardBackReducer";
 
-const rootReducer = combineReducers({
-  theme: themeReducer,
-  cardBack: cardBackReducer,
-});
+// const rootReducer = combineReducers({
+//   theme: themeReducer,
+//   cardBack: cardBackReducer,
+// });
 const persistConfig = {
   key: "root",
   storage,
