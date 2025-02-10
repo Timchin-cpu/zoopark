d87f7e0ed3b9df9fde505495ee1a680fdcaaa6d9
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-10 15:46:10 +0000

    modified:   src/redux/actions.js 	modified:   src/redux/reducer.js 	deleted:    src/redux/reducers/cardBackReducer.js 	modified:   src/redux/store.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,21 +1,17 @@
-// const initialState = {
-//   theme: true,
-//   cardBack: "default", // Добавить начальное состояние для cardBack
-// };
+const initialState = {
+  theme: true,
+};
 
-// const rootReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "SET_THEME":
-//       return {
-//         ...state,
-//         theme: action.payload,
-//       };
-//     case "SET_CARD_BACK":
-//       return {
-//         ...state,
-//         cardBack: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-// };
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_THEME":
+      return {
+        ...state,
+        theme: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,34 +2,7 @@ import { createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./reducer";
-const initialState = {
-  theme: true,
-  cardBack: "default", // Добавить начальное состояние для cardBack
-};
 
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "SET_THEME":
-      return {
-        ...state,
-        theme: action.payload,
-      };
-    case "SET_CARD_BACK":
-      return {
-        ...state,
-        cardBack: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-// import { combineReducers } from "redux";
-// import { cardBackReducer } from "./reducers/cardBackReducer";
-
-// const rootReducer = combineReducers({
-//   theme: themeReducer,
-//   cardBack: cardBackReducer,
-// });
 const persistConfig = {
   key: "root",
   storage,
