acd543975083447ca2b2db45e11fa55be9c7de87
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-10 15:36:23 +0000

    modified:   src/redux/reducer.js 	modified:   src/redux/store.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,17 +1,21 @@
 const initialState = {
   theme: true,
+  cardBack: "default", // Добавить начальное состояние для cardBack
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'SET_THEME':
+    case "SET_THEME":
       return {
         ...state,
         theme: action.payload,
+      };
+    case "SET_CARD_BACK":
+      return {
+        ...state,
+        cardBack: action.payload,
       };
     default:
       return state;
   }
 };
-
-export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
-import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import rootReducer from './reducer';
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./reducer";
+import { combineReducers } from "redux";
+import { cardBackReducer } from "./reducers/cardBackReducer";
 
+const rootReducer = combineReducers({
+  theme: themeReducer,
+  cardBack: cardBackReducer,
+});
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
 };
 
