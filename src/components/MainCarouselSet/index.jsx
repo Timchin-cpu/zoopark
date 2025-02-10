00ef0c9916d25de04bf74f0c2b4eddbe69832331
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-10 15:24:13 +0000

    modified:   src/components/MainCarouselSet/index.jsx

--- src/components/MainCarouselSet/index.jsx
+++ src/components/MainCarouselSet/index.jsx
@@ -15,7 +15,9 @@ import ReactFlipCard from "reactjs-flip-card";
 // import SettingsPopup from "components/SettingsPopup";
 import { peopleService } from "services/api";
 import { useSelector } from "react-redux";
-
+import DefaultImg from "assets/img/default-card.png";
+import Style1CardBack from "assets/img/card1.png";
+import Style2CardBack from "assets/img/card2.png";
 // Отсутствует определение cardBackStyles
 const cardBackStyles = {
   default: { image: DefaultImg },
