d9e7a03be32fcf8a8957c4c1e99fe750a1464984
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-02-13 21:59:51 +0000

    modified:   src/components/MainCarouselSet/index.jsx

--- src/components/MainCarouselSet/index.jsx
+++ src/components/MainCarouselSet/index.jsx
@@ -18,13 +18,14 @@ import { useSelector } from "react-redux";
 import DefaultImg from "assets/img/default-card.png";
 import Style1CardBack from "assets/img/card1.png";
 import Style2CardBack from "assets/img/card2.png";
+import DefaultImg1 from "assets/img/4210629.png";
+
 // Отсутствует определение cardBackStyles
 const cardBackStyles = {
   default: { image: DefaultImg },
   style1: { image: Style1CardBack },
   style2: { image: Style2CardBack },
 };
-import DefaultImg1 from "assets/img/4210629.png";
 // import ProdImg from "assets/img/prod-img.png";
 const data = [
   { id: 1, bgColor: "#F54748", title: "Slide 1" },
