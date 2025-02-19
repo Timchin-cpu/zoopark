fcc982528aaa1ec642028fcdeb9aab904c40173d
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-02-19 14:48:57 +0000

    modified:   src/pages/CardManagement/index.jsx

--- src/pages/CardManagement/index.jsx
+++ src/pages/CardManagement/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./CardManagement.module.css";
 import routeCardManagement from "./route";
 import { cardsService } from "services/api";
