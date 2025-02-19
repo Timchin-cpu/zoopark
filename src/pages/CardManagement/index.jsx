9c1c30431d06120c94e60075e8b674a67aff8eae
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-02-19 14:34:25 +0000

    modified:   src/pages/AdminPanel/index.jsx 	modified:   src/pages/CardManagement/index.jsx

--- src/pages/CardManagement/index.jsx
+++ src/pages/CardManagement/index.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import styles from "./CardManagement.module.css";
 import routeCardManagement from "./route";
-import { NavLink } from "react-router-dom";
-import { routeCardManagement } from "pages/CardManagement";
+
 const CardManagement = () => {
   return (
     <div className={styles.contents}>
-      <div className={styles.mainContent}>
-        <NavLink to={routeCardManagement()}>
-          <div className={styles.content}>
-            <p>Добавление/редактирование карт жители/город и наборов </p>
-          </div>
-        </NavLink>
-      </div>
+      <div className={styles.mainContent}></div>
     </div>
   );
 };
