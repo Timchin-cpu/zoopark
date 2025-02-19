7f93907fa89d73b64897491227b50ca012036b8f
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-02-19 13:56:50 +0000

    modified:   src/pages/AdminPanel/index.jsx

--- src/pages/AdminPanel/index.jsx
+++ src/pages/AdminPanel/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./AdminPanel.module.css";
+import routeAdmin from "./route";
+
 const AdminPanel = () => {
   return <div className={styles.qwa}></div>;
 };
