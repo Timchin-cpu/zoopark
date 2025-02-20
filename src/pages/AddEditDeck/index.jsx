d6f2567eb5e10207ff8aaa4ea8cec8150750e12c
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-02-20 16:54:05 +0000

    modified:   src/pages/AddEditDeck/index.jsx

--- src/pages/AddEditDeck/index.jsx
+++ src/pages/AddEditDeck/index.jsx
@@ -9,6 +9,19 @@ import routeAddEditDeck from "./route";
 // const [selectedCards, setSelectedCards] = useState([]);
 const { id } = useParams(); // Добавить импорт useParams из react-router-dom
 console.log(id);
+const [cards, setCards] = useState([]);
+
+useEffect(() => {
+  const fetchCards = async () => {
+    try {
+      const response = await cardsService.getAllCards();
+      setCards(response.data);
+    } catch (error) {
+      console.error("Error fetching cards:", error);
+    }
+  };
+  fetchCards();
+}, []);
 // const handleAddCard = async (cardId) => {
 //   try {
 //     await cardSetsService.addCardToSet(id, cardId);
@@ -17,15 +30,26 @@ console.log(id);
 //     console.error("Error adding card to set:", error);
 //   }
 // };
+const handleAddCardToSet = async (cardId, setId) => {
+  try {
+    await cardSetsService.addCardToSet(setId, cardId);
+  } catch (error) {
+    console.error("Error adding card to set:", error);
+  }
+};
 const AddEditDeck = () => {
   return (
     <div className={styles.contents}>
       <div className={styles.mainContent}>
-        {/* <NavLink to={routeAddEditDeck()}>
-          <div className={styles.content}>
-            <p>Добавление/редактирование карт жители/город и наборов </p>
+        {cards.map((card) => (
+          <div key={card.id} className={styles.cardItem}>
+            <img src={`http://localhost:3000${card.image}`} alt={card.title} />
+            <h3>{card.title}</h3>
+            <button onClick={() => handleAddCardToSet(card.id, setId)}>
+              Добавить в набор
+            </button>
           </div>
-        </NavLink> */}
+        ))}
       </div>
     </div>
   );
