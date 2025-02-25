49cfe533dcdaed2c089e190b407c2f52d0f10c1f
ArtFullstack <ve4tom@gmail.com>
2025-02-25 18:57:07 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -30,6 +30,10 @@ const data = [
   { id: 1, bgColor: "#F54748", title: "Slide 1" },
   { id: 2, bgColor: "#7952B3", title: "Slide 2" },
   { id: 3, bgColor: "#1597BB", title: "Slide 3" },
+  { id: 4, bgColor: "#1597BB", title: "Slide 3" },
+  { id: 5, bgColor: "#1597BB", title: "Slide 3" },
+  { id: 6, bgColor: "#1597BB", title: "Slide 3" },
+  { id: 7, bgColor: "#1597BB", title: "Slide 3" },
 ];
 const MainCarousel = ({ getActiveSlide, handleOpenPopup }) => {
   const [openedCards, setOpenedCards] = useState({});
