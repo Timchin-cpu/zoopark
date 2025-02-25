576dc79fc58319dfbe260152ff399fffee6bc88b
ArtFullstack <ve4tom@gmail.com>
2025-02-25 10:01:41 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -114,14 +114,14 @@ const MainCarousel = ({ getActiveSlide, handleOpenPopup }) => {
   };
 
   const handleImageClick = (index) => {
-    setActiveIndex(index === activeIndex ? null : index);
-    setOpenedCards({
-      ...openedCards,
-      [index]: selectedPhotos[data[index].id],
-    });
-    handleOpenPopup(selectedPhotos[data[index].id]);
+    const card = selectedPhotos[data[index].id];
+    const chance = card?.chance || 100; // Получаем шанс выпадения карты, по умолчанию 100%
+    if (Math.random() * 100 <= chance) {
+      // Проверяем, выпала ли карта согласно шансу
+      setActiveIndex(index === activeIndex ? null : index);
+      handleOpenPopup(card);
+    }
   };
-
   return (
     <div className="main-control">
       <div className="main-control__bg">
