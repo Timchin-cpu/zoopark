198ccafbedef71cdbedd3e331170c14976c10909
ArtFullstack <ve4tom@gmail.com>
2025-02-26 14:07:04 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -146,7 +146,7 @@ const MainCarousel = ({ getActiveSlide, handleOpenPopup }) => {
 
     setActiveIndex(null);
     setOpenedCards({});
-    setIsFlipped(false);
+    setIsFlipped(true);
   };
 
   return (
