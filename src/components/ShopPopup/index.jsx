93dcb2abc1437f41b6b7ceede8b81d844e82a04c
ArtFullstack <ve4tom@gmail.com>
2025-02-26 09:01:20 +0000

    modified:   src/components/ShopPopup/index.jsx

--- src/components/ShopPopup/index.jsx
+++ src/components/ShopPopup/index.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import ReactFlipCard from "reactjs-flip-card";
 import "./styles.scss";
 
 import DefaultImg from "assets/img/default-img.png";
