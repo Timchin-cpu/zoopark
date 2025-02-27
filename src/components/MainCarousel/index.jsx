6ba861dfcacf536f8bf55b60de10fcd244f2918f
ArtFullstack <ve4tom@gmail.com>
2025-02-27 14:14:07 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -205,6 +205,7 @@ const MainCarousel = ({
 
   const [isFlipped, setIsFlipped] = useState(false);
   const handleImageClick = async (index) => {
+    console.log(index, openedCards);
     const tg = window.Telegram.WebApp;
     const telegram_id = tg.initDataUnsafe?.user?.id;
 
