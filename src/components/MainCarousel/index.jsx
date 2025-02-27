52bcc9593d868a7ecf181dc634411cf5b714e424
ArtFullstack <ve4tom@gmail.com>
2025-02-27 10:52:34 +0000

    modified:   src/components/MainCarousel/index.jsx

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -68,6 +68,7 @@ const MainCarousel = ({
           const response = await userInitService.getEnergy(telegram_id);
           if (response.data && response.data.energy) {
             setEnergy(response.data.energy);
+            console.log(response.data.energy);
           }
         } catch (error) {
           console.error("Error fetching energy:", error);
