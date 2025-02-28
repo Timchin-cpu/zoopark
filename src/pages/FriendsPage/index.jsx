a8ae87832289a88b02df549c39473ab8b99b7515
ArtFullstack <ve4tom@gmail.com>
2025-02-28 13:44:15 +0000

    modified:   src/pages/FriendsPage/index.jsx

--- src/pages/FriendsPage/index.jsx
+++ src/pages/FriendsPage/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { userInitService } from "services/api";
 import routeFriends from "./routes";
 import MainSection from "components/MainSection";
 
