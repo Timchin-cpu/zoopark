c6ae492fdd66f94e99dbbcfe6b1e6b7ae99efef0
ArtFullstack <ve4tom@gmail.com>
2025-03-02 09:49:31 +0000

    modified:   src/pages/AddEditCard/index.jsx

--- src/pages/AddEditCard/index.jsx
+++ src/pages/AddEditCard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./AddEditCard.module.css";
 import routeAddEditCard from "./route";
 import { cardsService } from "services/api";
