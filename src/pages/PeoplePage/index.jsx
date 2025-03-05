3a05cde36a22e42be96e49fe72fb71d4f0010ddd
ArtFullstack <ve4tom@gmail.com>
2025-03-05 10:46:28 +0000

    modified:   src/pages/PeoplePage/index.jsx

--- src/pages/PeoplePage/index.jsx
+++ src/pages/PeoplePage/index.jsx
@@ -15,6 +15,7 @@ const PeoplePage = () => {
   const [showInfo, setShowInfo] = useState(false);
   const [policePhotos, setPolicePhotos] = useState([]);
   const [cardSets, setCardSets] = useState([]);
+  console.log(cardSets);
   const [selectedPhoto, setSelectedPhoto] = useState(null);
   const [activePopup, setActivePopup] = useState(false);
   const [userCards, setUserCards] = useState([]);
@@ -369,74 +370,6 @@ const PeoplePage = () => {
                 </div>
               </div>
             </li>
-            {cardSets.map((set) => (
-              <li key={set.id} className="city-list__item block-style">
-                <div
-                  className={`city-list__title f-center-jcsb ${
-                    openAccordion === set.id ? "active" : ""
-                  }`}
-                >
-                  {set.name}
-                  <div className="city-list__more f-center">
-                    <div className="city-list__count">
-                      {
-                        userCards.filter((card) => set.cards.includes(card.id))
-                          .length
-                      }{" "}
-                      из {set.cards.length}
-                    </div>
-                    <div
-                      className="city-list__arrow"
-                      onClick={() => handleAccordionClick(set.id)}
-                    >
-                      <svg width="15" height="9" viewBox="0 0 15 9" fill="none">
-                        <path
-                          d="M14.6592 1.56103L8.23438 8.13525C8.08496 8.29297 7.88574 8.38428 7.66992 8.38428C7.4624 8.38428 7.25488 8.29297 7.11377 8.13525L0.688966 1.56103C0.547853 1.42822 0.464845 1.2373 0.464845 1.02148C0.464845 0.589842 0.788575 0.266112 1.22022 0.266112C1.42774 0.266112 1.62695 0.340819 1.75977 0.481932L7.66992 6.5249L13.5884 0.481933C13.7129 0.34082 13.9121 0.266113 14.1279 0.266113C14.5596 0.266113 14.8833 0.589844 14.8833 1.02148C14.8833 1.2373 14.8003 1.42822 14.6592 1.56103Z"
-                          fill="#AAB2BD"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-                {/* Отображение карт набора */}
-                <div
-                  className="city-list__content"
-                  style={{
-                    maxHeight: openAccordion === set.id ? "500px" : "0px",
-                    paddingTop: openAccordion === set.id ? "24px" : "0px",
-                    paddingBottom: openAccordion === set.id ? "10px" : "0px",
-                  }}
-                >
-                  <div className="city-slider">
-                    <Swiper spaceBetween={8} slidesPerView={"auto"}>
-                      {set.cards.map((card) => (
-                        <SwiperSlide key={card.id}>
-                          <div className="city-slider__item">
-                            <div
-                              className="city-slider__card"
-                              onClick={() => handleOpenPopup(card)}
-                            >
-                              <p className="city-slider__image">
-                                <img
-                                  src={
-                                    userCards.some(
-                                      (userCard) => userCard.id === card.id
-                                    )
-                                      ? `http://localhost:3000${card.image}`
-                                      : QuestionMarkImg
-                                  }
-                                  alt={card.title}
-                                />
-                              </p>
-                            </div>
-                          </div>
-                        </SwiperSlide>
-                      ))}
-                    </Swiper>
-                  </div>
-                </div>
-              </li>
-            ))}
           </ul>
         </div>
       </div>
