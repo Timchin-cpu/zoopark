b57b79318f563727f5e31e31cedaebb35a6deeb0
ArtFullstack <ve4tom@gmail.com>
2025-03-06 15:08:05 +0000

    modified:   src/pages/AdminPanel/index.jsx

--- src/pages/AdminPanel/index.jsx
+++ src/pages/AdminPanel/index.jsx
@@ -6,12 +6,18 @@ import { routeCardManagement } from "pages/CardManagement";
 const AdminPanel = () => {
   return (
     <div className={styles.contents}>
-      <div className={styles.mainContent}>
-        <NavLink to={routeCardManagement()}>
-          <div className={styles.content}>
-            <p>Добавление/редактирование карт жители/город и наборов </p>
-          </div>
-        </NavLink>
+      <div className={styles.mainContents}>
+        {" "}
+        <div className={styles.mainContent}>
+          <NavLink to={routeCardManagement()}>
+            <div className={styles.content}>
+              <p>Добавление/редактирование карт жители/город и наборов </p>
+            </div>
+          </NavLink>
+        </div>
+        <div className={styles.mainContent}></div>
+        <div className={styles.mainContent}></div>
+        <div className={styles.mainContent}></div>
       </div>
     </div>
   );
