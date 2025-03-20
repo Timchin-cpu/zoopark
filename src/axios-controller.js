c3821257b6a2b49247a85d4b6eab5ab1c2d0b11e
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-03-20 18:10:47 +0000

    modified:   src/axios-controller.js

--- src/axios-controller.js
+++ src/axios-controller.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
 const instance = axios.create({
-  baseURL: "/api",
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'https://timchin-cpu-zoopark-4e00.twc1.net/api'
+    : '/api',
+  withCredentials: true
 });
-
 export default instance;
